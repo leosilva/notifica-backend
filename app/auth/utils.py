649c2ff398b921a46980f5766b853e3ed6895d3d
Yuri Teixeira <yuriteixeirac@proton.me>
2026-08-25 16:58:54 -0300

    fix: remoção de função inutilizada

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,13 +1,6 @@
 import os
 
 from dotenv import load_dotenv
-from flask import redirect
-from flask_jwt_extended import (
-    create_access_token,
-    create_refresh_token,
-    set_access_cookies,
-    set_refresh_cookies,
-)
 
 from app.auth.models import Role
 
